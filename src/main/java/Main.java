6fb5680f05dd6ee0784d03c3709023197d7ca3b6
kudongku <kudongku@naver.com>
2024-09-27 23:02:42 +0900

    feat : question01 solved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
+
+    public static void main(String[] args) {
+        int[] answer01 = Question01.solution(new int[]{4,3,2,1});
+        int[] answer02 = Question01.solution(new int[]{10});
+        int[] answer03 = Question01.solution(new int[]{5,3,1,4,2});
+
+        System.out.println("answer01 = " + Arrays.toString(answer01));
+        System.out.println("answer02 = " + Arrays.toString(answer02));
+        System.out.println("answer03 = " + Arrays.toString(answer03));
+    }
 
 }
--- src/main/java/Question01.java
+++ src/main/java/Question01.java
@@ -1,3 +1,27 @@
 public class Question01 {
 
+    public static int[] solution(int[] args) {
+        int length = args.length;
+        int minValue = args[0];
+        int[] returnArray = new int[length - 1];
+
+        if (length == 1) {
+            return new int[]{-1};
+        }
+
+        for (int i : args) {
+            if (i < minValue) {
+                minValue = i;
+            }
+        }
+
+        for (int j = 0, pointer = 0; j < length; j++) {
+            if (args[j] != minValue) {
+                returnArray[pointer++] = args[j];
+            }
+        }
+
+        return returnArray;
+    }
+
 }
